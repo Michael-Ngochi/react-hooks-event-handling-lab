34642986876a6ad40d298ae80833da1f4294315b
Michael-Ngochi <ngoochimike@gmail.com>
2025-04-11 11:49:33 +0300

    added event handlers

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe (){
+
+function handleFocus(event){
+console.log("Good!")
+}
+
+function handleBLur(event){
+console.log("Hey! Eyes on me!")
+}
+
+
+
+
+return(
+    <button onFocus={handleFocus} onBlur={handleBLur}>Eyes on me</button>
+)
+}
+
+export default EyesOnMe;
